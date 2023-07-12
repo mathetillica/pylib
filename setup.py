401cf8667445dd3cda3a3a66788ed40ae6bfcbb5
mathetillica <ujjawalcrazy2002@gmail.com>
2023-07-12 22:10:17 +0530

    package name changed

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import setup
 
 with open("README.md", "r") as f:
     long_description =f.read()
-setup( name="bestPR1",
-       version="0.6.9",
+setup( name="PypolyBest",
+       version="0.6.11",
+       description="see long description",
        long_description=long_description,
        long_description_content_type="text/markdown",
        author="Ujjawal",
        url = "",
        author_email="ujjawald21@iitk.ac.in",
-       packages=['bestPR'],
+       packages=['PypolyBest'],
        install_requires=['scikit-learn'],
        classifiers=[
            "Programming Language :: Python :: 3.8",
